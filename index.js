38d5f0b38f76abae7a30e191183d22601b6bb4be
gdreamer1 <gdadreamer@gmail.com>
2021-03-16 17:01:56 -0700

    Completed Assinments

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+console.log(cats);
+       
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+//ralph^^
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}//bob^^
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name,...cats]
+}
+
+function removeLastCat() {
+    return cats.slice(0,2)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
